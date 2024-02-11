83a3623e3e27c3fa14a7f0cd392dc7d618dd44af
ryrichard <66878091+ryrichard@users.noreply.github.com>
2024-02-11 16:52:53 -0500

    Added login

--- frontend/app/index.jsx
+++ frontend/app/index.jsx
@@ -1,17 +1,27 @@
-import { Pressable, Text, View } from "react-native";
+import { Pressable, Text, View, StyleSheet } from "react-native";
 import { Link, router } from "expo-router";
+import Login from './screens/Login/Login';
 
 const Homepage = () => {
     const id = 1
     return (
-        <View>
+        <View style={styles.root}>
             <Text>Homepage</Text>
             <Link href="/Map/MapPage">Map</Link>
-            <Pressable onPress={() => router.push(`/user/${id}`)}>
+            <Pressable onPress={() => router.push(`/screens/Login/Login/`)}>login</Pressable>
+            <Pressable onPress={() => router.push(`/screens/Register/Register/`)}>register</Pressable>
+            <Pressable onPress={() => router.push(`/screens/User/${id}`)}>
                 <Text>User 1</Text>
             </Pressable>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    root: {
+        flex: 1,
+        backgroundColor: '#F9FBFC'
+    }
+})
 
 export default Homepage;
