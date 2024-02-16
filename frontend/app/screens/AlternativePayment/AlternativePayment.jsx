bdf566d25b0f4c30adbebdff7f9b49c2224351b4
ryrichard <66878091+ryrichard@users.noreply.github.com>
2024-02-16 16:52:06 -0500

    Added more pages

---
+++ frontend/app/screens/AlternativePayment/AlternativePayment.jsx
@@ -0,0 +1,11 @@
+import {View, Text, StyleSheet} from 'react-native';
+
+const AlternativePayment = () => {
+    return (
+        <View>
+            <Text>Input Medical Care</Text>
+        </View>
+    )
+}
+
+export default AlternativePayment; 
---
+++ frontend/app/screens/Chatbot/Chatbot.jsx
@@ -0,0 +1,11 @@
+import {View, Text, StyleSheet} from 'react-native';
+
+const Chatbot = () => {
+    return (
+        <View>
+            <Text>Chatbot</Text>
+        </View>
+    )
+}
+
+export default Chatbot;
