b7694f1a4400e44786fb9b1411b7c6e9683f788e
mrahman4782 <moharahman50@gmail.com>
2024-02-29 16:39:33 -0500

    Frontend API server call for chatgpt

---
+++ backend/functions/server.js
@@ -0,0 +1,49 @@
+
+import express from 'express';
+import cors from 'cors';
+
+import initializeFirebaseApp from './firebaseAdminInit.js';
+initializeFirebaseApp();
+
+import {createUser} from './register.js';
+import {loginVerify} from './loginHandler.js';
+
+
+
+const app = express();
+const port = 3000;
+app.use(cors());
+
+app.use(express.json());
+
+// GET Requests
+app.get('/', (req, res) => {
+    res.status(200).send(`<h1>Successfully Connected to Server</h1>`)
+})
+
+
+// POST Requests
+app.post('/api/verifyLogin', async(req, res) => {
+
+    console.log(req);
+    let token = req.body.token;
+    let checkUserLogin = await loginVerify(token);
+    
+    console.log(checkUserLogin);
+    res.status(checkUserLogin.status).send(`Logged in! Expiration time: ${checkUserLogin.data.exp}`);
+    
+})
+
+app.post('/api/registerUser', async(req, res) => {
+
+    console.log(req);
+    let email = req.body.email;
+    let password = req.body.password;
+    let checkUserLogin = await createUser(email, password);
+    
+    console.log(checkUserLogin);
+    res.status(checkUserLogin.status).send(`Registered!`);
+    
+})
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
