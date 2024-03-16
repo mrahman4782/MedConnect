90245b9171316b1add19837161f2422717210cc7
lee <k205leeyh@gmail.com>
2024-03-17 07:01:48 +0900

    "Add scrolldown when new input"

--- frontend/app/screens/Chatbot/Chatbot.jsx
+++ frontend/app/screens/Chatbot/Chatbot.jsx
@@ -1,15 +1,20 @@
-import { Text, View, ImageBackground, StyleSheet, useWindowDimensions, ScrollView } from "react-native";
+import { Text, View, ImageBackground, StyleSheet, useWindowDimensions, ScrollView  } from "react-native";
 import Logo from '../../../assets/icon.png';
 import CustomInput from "../../components/CustomInput";
 import CustomButton from '../../components/CustomButton';
 import chatWithGPT from '../../functions/apiCall';
-import { useState, useRef } from 'react';
+import { useState, useRef, useEffect } from 'react';
 
 const Chatbot = () => {
     const { height } = useWindowDimensions();
     const [input, setInput] = useState("");
     const [gptOutput, setGptOutput] = useState([{ type: 'gpt', text: 'Hello!' }]); // store chat history
     const scrollViewRef = useRef(); // manage scroll for scrollView
+
+    useEffect(() => {   //Scroll down, input new message 
+        scrollViewRef.current.scrollToEnd({ animated: true });
+    }, [gptOutput]);
+
 
     const styles = StyleSheet.create({
         scrollViewContainer: {
@@ -22,15 +27,6 @@ const Chatbot = () => {
             width: '100%',
             minHeight: height,
             backgroundColor: '#b0c2e8',
-        },
-        textBox: {
-            justifyContent: 'flex-end',
-            alignItems: 'center',
-            paddingBottom: 20,
-            width: '100%',
-            position: "absolute",
-            bottom: 5,
-            zIndex: 10,
         },
         messageContainer: {
             flexDirection: 'row',
@@ -45,21 +41,41 @@ const Chatbot = () => {
             borderTopRightRadius: 20,
             borderBottomRightRadius: 20,
             borderTopLeftRadius: 20,
-            marginRight: '95%',
-            alignSelf: 'flex-end',
+
         },
         gptMessage: {
             backgroundColor: '#90ee90',
             borderTopLeftRadius: 20,
             borderBottomLeftRadius: 20,
             borderTopRightRadius: 20,
-            marginLeft: '20%',
-            alignSelf: 'flex-start',
+
         },
         messageText: {
             padding: 10,
             color: 'black',
         },
+        inputContainer: {
+            flexDirection: 'row',
+            width: '80%',
+            borderColor: '#000', 
+            padding: 10,
+        },
+        textInput: {
+            flex: 1, 
+            marginRight: 10,
+            fontSize: 18, 
+        },
+        sendButtonContainer: {
+            backgroundColor: '#000', 
+            borderRadius: 5, 
+        },
+        sendButtonText: {
+            width: 30,
+            color: '#fff', 
+            fontWeight: 'bold',
+
+        },
+
     });
 
     //Function when you press 'Enter', Send user message to GPT and append answer
@@ -67,7 +83,6 @@ const Chatbot = () => {
         if (input !== '') {
             const newGptOutput = { type: 'user', text: input }; // preapre to user message for display
             setGptOutput([...gptOutput, newGptOutput]); // add user message to chat history
-
             const output = await chatWithGPT(input); // send user message to GPT and waiting
             setGptOutput(currentGptOutput => [...currentGptOutput, { type: 'gpt', text: output }]); // Add GPT answer in to chat history
             setInput(""); // Clear input area after send message
@@ -75,21 +90,28 @@ const Chatbot = () => {
     };
 
     return (
-        <ScrollView style={styles.scrollViewContainer} showsVerticalScrollIndicator={false} ref={scrollViewRef}>
+        <ScrollView 
+        style={styles.scrollViewContainer} 
+        contentContainerStyle={styles.contentContainer}
+        showsVerticalScrollIndicator={false}
+         ref={scrollViewRef}>
             <ImageBackground source={Logo} style={styles.background} resizeMode="cover">
                 <Text>Welcome to the MedChat</Text>
                 {gptOutput.map((msg, index) => (    // Iterate chat history and display
-                    <View key={index} style={[msg.type === 'user' ? styles.userMessage : styles.gptMessage, styles.messageContainer]}>
+                    <View key={index} style={[styles.messageContainer, msg.type === 'user' ? styles.userMessage : styles.gptMessage, { alignSelf: msg.type === 'user' ? 'flex-end' : 'flex-start' }]}>
                         <Text style={styles.messageText}>{msg.text}</Text>
                     </View>
                 ))}
-                <View style={styles.textBox}>
+                <View style={styles.inputContainer}>
                     <CustomInput
+                        style={styles.textInput}
                         placeholder="Enter your message"
                         value={input}
                         setValue={setInput}
                     />
                     <CustomButton
+                        containerStyle={styles.sendButtonContainer}
+                        textStyle={styles.sendButtonText}
                         text="Enter"
                         onPress={onEnterPress}
                     />
