01ef254a2c96911a96e31ecc890d2972f4148786
mrahman4782 <moharahman50@gmail.com>
2024-04-11 10:37:21 -0400

    Removed map errors from UI & functionality updated for Register

--- frontend/app/functions/login.js
+++ frontend/app/functions/login.js
@@ -43,11 +43,11 @@ export async function userLogin(email, password) {
   } catch (error) {
     const errorCode = error.code;
     const errorMessage = error.message;
-    console.log(errorCode);
-    console.log(errorMessage);
-    console.log('AHHHHHHHHHHHHHHHHHhh');
-
-    return error;
+    console.log("Error Code: ", errorCode);
+    console.log("Error Msg: ", errorMessage);
+    // console.log('AHHHHHHHHHHHHHHHHHhh');
+    // return error;
+    throw error;
   }
 }
 
--- frontend/app/screens/Login/Login.jsx
+++ frontend/app/screens/Login/Login.jsx
@@ -1,9 +1,9 @@
 import { useState } from 'react'
-import { Pressable, Text, View, Image, StyleSheet, useWindowDimensions, ScrollView } from "react-native";
+import { Platform, Alert, Pressable, Text, View, Image, StyleSheet, useWindowDimensions, ScrollView } from "react-native";
 import Logo from '../../../assets/icon.png';
 import CustomInput from "../../components/CustomInput";
 import CustomButton from '../../components/CustomButton';
-import { useRouter } from 'expo-router';
+import { useRouter, Stack } from 'expo-router';
 import userLogin from '../../functions/login.js';
 import { Link, router } from "expo-router";
 import sessionStorage from '../../functions/sessionStorage';
@@ -13,30 +13,52 @@ const Login = () => {
 
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const [message, setMessage] = useState("");
+    const [errorMessage, setErrorMessage] = useState("");
 
     const { height } = useWindowDimensions();
 
     const onSignInPressed = async () => {
 
-        if (email != '' && password != '') {
+        // if (email != '' && password != '') {
 
-            let output = await userLogin(email, password);
+        //     let output = await userLogin(email, password);
 
-            if ('status' in output) {
-                if (output.status == 200) {
-                    let key = sessionStorage.sessionKey;
-                    setMessage(`SessionKey: ", ${key}`)
-                    router.push({
-                        pathname: "/",
-                    })
+        //     if ('status' in output) {
+        //         if (output.status == 200) {
+        //             let key = sessionStorage.sessionKey;
+        //             setMessage(`SessionKey: ", ${key}`)
+        //             router.push({
+        //                 pathname: "/",
+        //             })
 
+        //         }
+        //     }
+
+        // }
+        if (email === "" || password === "") {
+            setErrorMessage("Missing Input");
+        } else {
+            try {
+                await userLogin(email, password);
+                router.push({
+                    pathname: "/",
+                });
+                return;
+            } catch (error) {
+                if (error.code === "auth/invalid-email") {
+                    setErrorMessage('Invalid Login');
+                } else {
+                    setErrorMessage('An error occurred during login');
                 }
             }
-
         }
 
+        // Show alert if there's an error message
+        if (errorMessage && (Platform.OS === 'ios' || Platform.OS === 'android')) {
+            Alert.alert(errorMessage);
+        }
     }
+
 
     const onForgotPasswordPressed = () => {
         console.warn("Forgotpassword Pressed");
@@ -47,10 +69,15 @@ const Login = () => {
     }
 
     return (
-        <ScrollView showsVerticalScrollIndicator={false}>
+        <ScrollView showsVerticalScrollIndicator={false} style={styles.scrollView}>
+            <Stack.Screen
+                options={{
+                    headerTitle: 'HeaderTitle',
+                    headerShown: false, //change if you want
+                }}
+            />
             <View style={styles.root}>
-                <View style={styles.box}>
-
+                <View style={[styles.box, errorMessage ? styles.boxWithErrorMessage : null]}>
                     {/* <Image
                         source={Logo}
                         style={[styles.logo, { height: height * 0.3 }]}
@@ -85,7 +112,7 @@ const Login = () => {
                         type="TERTIARY"
                     />
 
-                    {message !== "" && <Text>{message}</Text>}
+                    {Platform.OS !== 'android' && Platform.OS !== 'ios' && errorMessage !== "" && <Text style={styles.errorMessage}>{errorMessage}</Text>}
                 </View>
 
             </View>
@@ -94,13 +121,18 @@ const Login = () => {
 }
 
 const styles = StyleSheet.create({
+    scrollView: {
+        flex: 1,
+        backgroundColor: '#000080',
+    },
     root: {
+        flex: 1,
+        justifyContent: 'center',
         alignItems: 'center',
-        flex: 1, 
-        padding: 20,
-        backgroundColor: '#e0e1dd',
-        paddingBottom: 130,
-        paddingTop: 80
+        padding: 200,
+        // backgroundColor: '#e0e1dd',
+        // paddingBottom: 130,
+        // paddingTop: 80,
     },
     box: {
         backgroundColor: '#598392',
@@ -108,19 +140,22 @@ const styles = StyleSheet.create({
         width: 300,
         height: 400,
         borderRadius: 20,
-        alignContent: 'center'
-    }, 
+        alignContent: 'center',
+    },
+    boxWithErrorMessage: {
+        height: 450,
+    },
     loginText: {
-        
+
         paddingLeft: 5,
         fontSize: 22,
-        
+
         color: "white",
         marginTop: 10,
         marginBottom: 50,
         paddingBottom: 5,
         textAlign: 'center',
-        
+
     },
     textFields: {
         padding: 5,
@@ -132,6 +167,14 @@ const styles = StyleSheet.create({
     },
     smallText: {
 
+    },
+    errorMessage: {
+        backgroundColor: 'red',
+        color: 'white',
+        paddingTop: '10px',
+        paddingBottom: '10px',
+        textAlign: 'center',
+        justifyContent: 'center'
     }
 
 })
