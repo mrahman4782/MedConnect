316c705ba0b461ad90bf8bd0bed69c6d82e13983
Namgyal <sasnams.e.sxd162001@gmail.com>
2024-04-11 08:51:06 -0400

    kept mohammed's OpenAI.js as it is, but removed the connection in apiCall.js. Added assistantai.js in functions folder of backend, then made necessary updates in the server.js and apicall.js

--- backend/functions/server.js
+++ backend/functions/server.js
@@ -1,73 +1,74 @@
+import express from "express";
+import cors from "cors";
 
-import express from 'express';
-import cors from 'cors';
-
-import initializeFirebaseApp from './firebaseAdminInit.js';
+import initializeFirebaseApp from "./firebaseAdminInit.js";
 initializeFirebaseApp();
 
-import {createUser} from './register.js';
-import {loginVerify} from './loginHandler.js';
-import {chat} from './openAI.js';
-import {updateProfile} from './updateProfile.js';
+import { createUser } from "./register.js";
+import { loginVerify } from "./loginHandler.js";
+import { chat } from "./openAI.js";
+import { updateProfile } from "./updateProfile.js";
+import { assistantAI } from "./assistantai.js";
 
 const app = express();
 const port = 3000;
 app.use(cors());
 
-app.use(express.json()); //middleware to parse json 
+app.use(express.json()); //middleware to parse json
 
 // GET Requests
-app.get('/', (req, res) => {
-    res.status(200).send(`<h1>Successfully Connected to Server</h1>`)
-})
-
+app.get("/", (req, res) => {
+  res.status(200).send(`<h1>Successfully Connected to Server</h1>`);
+});
 
 // POST Requests
-app.post('/api/verifyLogin', async(req, res) => {
+app.post("/api/verifyLogin", async (req, res) => {
+  console.log(req);
+  let token = req.body.token;
+  let checkUserLogin = await loginVerify(token);
 
-    console.log(req);
-    let token = req.body.token;
-    let checkUserLogin = await loginVerify(token);
-    
-    console.log(checkUserLogin);
-    res.status(checkUserLogin.status).send(`Logged in! Expiration time: ${checkUserLogin.data.exp}`);
-    
-})
+  console.log(checkUserLogin);
+  res
+    .status(checkUserLogin.status)
+    .send(`Logged in! Expiration time: ${checkUserLogin.data.exp}`);
+});
 
-app.post('/api/registerUser', async(req, res) => {
+app.post("/api/registerUser", async (req, res) => {
+  console.log(req);
+  let email = req.body.email;
+  let password = req.body.password;
+  let checkUserLogin = await createUser(email, password);
 
-    console.log(req);
-    let email = req.body.email;
-    let password = req.body.password;
-    let checkUserLogin = await createUser(email, password);
-    
-    console.log(checkUserLogin);
-    res.status(checkUserLogin.status).send(`Registered!`);
-    
-})
+  console.log(checkUserLogin);
+  res.status(checkUserLogin.status).send(`Registered!`);
+});
 
-app.post('/api/chat', async(req, res) => {
+app.post("/api/chat", async (req, res) => {
+  console.log(req);
+  let message = req.body.message;
+  let checkAPIOutput = await chat(message);
 
-    console.log(req);
-    let message = req.body.message;
-    let checkAPIOutput = await chat(message);
-    
-    console.log(checkAPIOutput);
-    res.status(checkAPIOutput.status).send(checkAPIOutput.data);
+  console.log(checkAPIOutput);
+  res.status(checkAPIOutput.status).send(checkAPIOutput.data);
+});
 
-})
+app.post("/api/assistantai", async (req, res) => {
+  let message = req.body.message;
+  let checkAPIOutput = await assistantAI(message);
 
-app.post('/api/updateUser', async(req, res) => {
+  res.status(checkAPIOutput.status).send(checkAPIOutput.data);
+});
 
-    console.log(req);
-    let token = req.body.token;
-    let data = req.body.data;
+app.post("/api/updateUser", async (req, res) => {
+  console.log(req);
+  let token = req.body.token;
+  let data = req.body.data;
 
-    let returnMessage = await updateProfile(data, token);
-    //let checkUserLogin = await loginVerify(token);
+  let returnMessage = await updateProfile(data, token);
+  //let checkUserLogin = await loginVerify(token);
 
-    console.log(returnMessage);
-    res.status(returnMessage.status).send(returnMessage.data);
-})
+  console.log(returnMessage);
+  res.status(returnMessage.status).send(returnMessage.data);
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
