d8580c2db30f511dd4f45ab50c6998b78c804eeb
ryrichard <66878091+ryrichard@users.noreply.github.com>
2024-04-28 14:10:08 -0400

    UpdateCredit

--- frontend/app/screens/Credit/Credit.jsx
+++ frontend/app/screens/Credit/Credit.jsx
@@ -45,88 +45,78 @@ const Credit = () => {
                     />
                 </View>
 
-                <View style={styles.bodyContainer}>
-                    <View style={styles.purposeContainer}>
-                        <Text style={{ textAlign: 'center' }}>The US healthcare system is increasingly burdensome and costly. Medconnect offers a solution by providing a one-stop platform for medical services. Our AI offers reliable medical information, while our integration with Zoc Doc and Google Maps simplifies booking appointments with medical professionals and pharmacies. With Medconnect, access to healthcare is streamlined in one convenient app</Text>
-                        <TouchableOpacity onPress={() => onHandlePress('https://github.com/mrahman4782/MedConnect')}>
-                            <Text style={{ color: 'blue', textDecorationLine: 'underline' }}>Github</Text>
+                <View style={styles.purposeContainer}>
+                    <Text style={styles.purposeText}>The US healthcare system is increasingly burdensome and costly. Medconnect offers a solution by providing a one-stop platform for medical services. Our AI offers reliable medical information, while our integration with Zoc Doc and Google Maps simplifies booking appointments with medical professionals and pharmacies. With Medconnect, access to healthcare is streamlined in one convenient app</Text>
+                    <TouchableOpacity onPress={() => onHandlePress('https://github.com/mrahman4782/MedConnect')}>
+                        <Text style={styles.linkText}>Github</Text>
+                    </TouchableOpacity>
+                </View>
+
+
+                <View style={styles.userRightContainer}>
+                    <Image
+                        source={MR}
+                        style={styles.userImage}
+                    />
+                    <View>
+                        <Text style={{ fontSize: 20, fontWeight: 'bold' }}>Mohammad Rahman</Text>
+                        <Text>Backend, Firebase, ZocDoc</Text>
+                        <TouchableOpacity onPress={() => onHandlePress('https://github.com/mrahman4782')}>
+                            <Text style={styles.linkText}>Github</Text>
                         </TouchableOpacity>
                     </View>
                 </View>
 
-                <View style={styles.bodyRightContainer}>
-                    <View style={styles.userRightContainer}>
-                        <Image
-                            source={MR}
-                            style={styles.userImage}
-                        />
-                        <View>
-                            <Text style={{ fontSize: 20, fontWeight: 'bold' }}>Mohammad Rahman</Text>
-                            <Text>Backend, Firebase, ZocDoc</Text>
-                            <TouchableOpacity onPress={() => onHandlePress('https://github.com/mrahman4782')}>
-                                <Text style={{ color: 'blue', textDecorationLine: 'underline' }}>Github</Text>
-                            </TouchableOpacity>
-                        </View>
+                <View style={styles.userLeftContainer}>
+                    <View>
+                        <Text style={{ fontSize: 20, fontWeight: 'bold' }}>Namgyal Thily</Text>
+                        <Text style={{ textAlign: 'right' }}>Chatbot AI</Text>
+                        <TouchableOpacity onPress={() => onHandlePress('https://github.com/Namgyal9')}>
+                            <Text style={{ ...styles.linkText, textAlign: 'right' }}>Github</Text>
+                        </TouchableOpacity>
+                    </View>
+                    <Image
+                        source={NT}
+                        style={styles.userImage}
+                    />
+                </View>
+                <View style={styles.userRightContainer}>
+                    <Image
+                        source={EP}
+                        style={styles.userImage}
+                    />
+                    <View>
+                        <Text style={{ fontSize: 20, fontWeight: 'bold' }}>Elizabeth Palacios</Text>
+                        <Text>Backend, Frontend UI, Map</Text>
+                        <TouchableOpacity onPress={() => onHandlePress('https://github.com/elypalacios')}>
+                            <Text style={styles.linkText}>Github</Text>
+                        </TouchableOpacity>
                     </View>
                 </View>
-                <View style={styles.bodyLeftContainer}>
-                    <View style={styles.userLeftContainer}>
-                        <View>
-                            <Text style={{ fontSize: 20, fontWeight: 'bold' }}>Namgyal Thily</Text>
-                            <Text style={{ textAlign: 'right' }}>Chatbot AI</Text>
-                            <TouchableOpacity onPress={() => onHandlePress('https://github.com/Namgyal9')}>
-                                <Text style={{ textAlign: 'right', color: 'blue', textDecorationLine: 'underline' }}>Github</Text>
-                            </TouchableOpacity>
-                        </View>
-                        <Image
-                            source={NT}
-                            style={styles.userImage}
-                        />
+                <View style={styles.userLeftContainer}>
+                    <View >
+                        <Text style={{ fontSize: 20, fontWeight: 'bold' }}>Yeongho Lee</Text>
+                        <Text style={{ textAlign: 'right' }}>Chat Page</Text>
+                        <TouchableOpacity onPress={() => onHandlePress('https://github.com/k205leeyh')}>
+                            <Text style={{ ...styles.linkText, textAlign: 'right' }}>Github</Text>
+                        </TouchableOpacity>
                     </View>
+                    <Image
+                        source={YL}
+                        style={styles.userImage}
+                    />
                 </View>
-                <View style={styles.bodyRightContainer}>
-                    <View style={styles.userRightContainer}>
-                        <Image
-                            source={EP}
-                            style={styles.userImage}
-                        />
-                        <View>
-                            <Text style={{ fontSize: 20, fontWeight: 'bold' }}>Elizabeth Palacios</Text>
-                            <Text>Backend, Frontend UI, Map</Text>
-                            <TouchableOpacity onPress={() => onHandlePress('https://github.com/elypalacios')}>
-                                <Text style={{ color: 'blue', textDecorationLine: 'underline' }}>Github</Text>
-                            </TouchableOpacity>
-                        </View>
-                    </View>
-                </View>
-                <View style={styles.bodyLeftContainer}>
-                    <View style={styles.userLeftContainer}>
-                        <View >
-                            <Text style={{ fontSize: 20, fontWeight: 'bold' }}>Yeongho Lee</Text>
-                            <Text style={{ textAlign: 'right' }}>Chat Page</Text>
-                            <TouchableOpacity onPress={() => onHandlePress('https://github.com/k205leeyh')}>
-                                <Text style={{ textAlign: 'right', color: 'blue', textDecorationLine: 'underline' }}>Github</Text>
-                            </TouchableOpacity>
-                        </View>
-                        <Image
-                            source={YL}
-                            style={styles.userImage}
-                        />
-                    </View>
-                </View>
-                <View style={styles.bodyRightContainer}>
-                    <View style={styles.userRightContainer}>
-                        <Image
-                            source={RY}
-                            style={styles.userImage}
-                        />
-                        <View>
-                            <Text style={{ fontSize: 20, fontWeight: 'bold' }}>Richard Yeung</Text>
-                            <Text>Frontend, Debugging</Text>
-                            <TouchableOpacity onPress={() => onHandlePress('https://github.com/ryrichard')}>
-                                <Text style={{ color: 'blue', textDecorationLine: 'underline' }}>Github</Text>
-                            </TouchableOpacity>
-                        </View>
+                <View style={styles.userRightContainer}>
+                    <Image
+                        source={RY}
+                        style={styles.userImage}
+                    />
+                    <View>
+                        <Text style={{ fontSize: 20, fontWeight: 'bold' }}>Richard Yeung</Text>
+                        <Text>Frontend, Debugging</Text>
+                        <TouchableOpacity onPress={() => onHandlePress('https://github.com/ryrichard')}>
+                            <Text style={styles.linkText}>Github</Text>
+                        </TouchableOpacity>
                     </View>
                 </View>
 
@@ -141,7 +131,7 @@ const styles = StyleSheet.create({
         backgroundColor: '#F5F8FE',
     },
     root: {
-        justifyContent: 'center',
+        alignItems: 'center',
         flex: 1,
         padding: 10,
     },
@@ -151,8 +141,9 @@ const styles = StyleSheet.create({
     },
     headerContainer: {
         // backgroundColor: 'pink',
-        justifyContent: 'center',
+        // justifyContent: 'center',
         alignItems: 'center',
+        marginBottom: 20,
     },
     aboutText: {
         fontSize: 24,
@@ -170,55 +161,60 @@ const styles = StyleSheet.create({
     },
     bodyLeftContainer: {
         // backgroundColor: 'blue',
+        marginLeft: 100,
         justifyContent: 'flex-start',
         alignItems: 'flex-start',
     },
     bodyRightContainer: {
         // backgroundColor: 'green',
+        marginRight: 100,
         justifyContent: 'flex-start',
         alignItems: 'flex-end',
     },
     userImage: {
-        width: 70,
-        height: 70,
+        width: 110,
+        height: 110,
         margin: 10,
     },
     purposeContainer: {
         backgroundColor: 'grey',
         padding: 10,
         borderRadius: 20,
-        // margin: 10,
-        height: 150,
-        width: '100%',
+        marginVertical: 10,
+        width: '90%', // Adjust width to fit different screen sizes
         alignItems: 'center',
-        justifyContent: 'center',
+        justifyContent: 'center', // Center content vertically
+    },
+    purposeText: {
+        fontSize: 20,
+        textAlign: 'center',
+        marginVertical: 10,
     },
     userRightContainer: {
+        alignItems: 'center',
         backgroundColor: '#5483B3',
         padding: 20,
         borderRadius: 20,
         marginVertical: 10,
-        marginHorizontal: 200,
-        height: 200,
-        width: '68%',
-        alignItems: 'center',
-        justifyContent: 'flex-end',
-        flexDirection: 'row'
+        width: '60%', // Set width to about half the screen
+        alignSelf: 'flex-end', // Align the container to the far right
+        flexDirection: 'row',
+        justifyContent: 'flex-end', // Align 
     },
     userLeftContainer: {
         backgroundColor: 'rgba(5, 38, 89, 0.8)',
         padding: 20,
         borderRadius: 20,
         marginVertical: 10,
-        marginHorizontal: 200,
-        height: 200,
-        width: '68%',
+        width: '60%',
         alignItems: 'center',
+        alignSelf: 'flex-start',
         justifyContent: 'flex-start',
         flexDirection: 'row'
     },
-    userText: {
-
+    linkText: {
+        color: 'blue',
+        textDecorationLine: 'underline',
     },
 });
 
